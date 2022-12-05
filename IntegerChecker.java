415dd8ee874497b0b387c81bcc2f365ade0893bf
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-05 22:55:22 +0530

    Add files via upload

---
+++ IntegerChecker.java
@@ -0,0 +1,10 @@
+package CarryQuestion1;
+import java.util.Scanner;
+public class IntegerChecker {
+public static void main(String[] args) {
+	System.out.println("Enter your number");
+	Scanner sc = new Scanner(System.in);
+	System.out.println(sc.hasNextInt());
+	
+}
+}
