8398df63dfe59ccf17fab27f1649a1b090175bd2
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-05 22:57:00 +0530

    Add files via upload

---
+++ NameGreet.java
@@ -0,0 +1,11 @@
+package CarryQuestion1;
+import java.util.Scanner;
+public class NameGreet {
+public static void main(String[] args) {
+	System.out.println("Name greet");
+	Scanner sc = new Scanner(System.in);
+	System.out.println("What is your name");
+	String a = sc.next();
+	System.out.println("HEllo "  +  a  +  " have a good day");
+}
+}
