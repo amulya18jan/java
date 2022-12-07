72e2b2e7c7ea3ed75eb2b5c8e7ae8529778a005f
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-07 23:01:17 +0530

    Add files via upload

---
+++ ResultExprees.java
@@ -0,0 +1,8 @@
+package CarryQuestion1;
+import java.util.Scanner;
+public class ResultExprees {
+public static void main(String[] args) {
+	float a = 7/4.0f * 9/2.0f;
+	System.out.println(a);
+}
+}
