af9c3c8cfefe5772c6ae2adc437d8961d1a1a5f0
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-09 23:37:34 +0530

    Add files via upload

---
+++ LowerCase.java
@@ -0,0 +1,11 @@
+package CarryQuestion1;
+
+public class LowerCase {
+public static void main(String[] args) {
+	
+
+String name = "Amulya";
+name = name.toLowerCase();
+System.out.println(name);
+}
+}
