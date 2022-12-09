a4f690659eb0f920d0a4f79feb6a18f9853fd041
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-09 23:36:03 +0530

    Add files via upload

---
+++ Replacing.java
@@ -0,0 +1,9 @@
+package CarryQuestion1;
+
+public class Replacing {
+public static void main(String[] args) {
+	String name ="Amu ya";
+name = name.replace(" ","_");
+System.out.println(name);
+}
+}
