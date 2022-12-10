daa56628c8a6142255b17327adeb15b020b07b28
Amulya Awasthi <113835162+amulya18jan@users.noreply.github.com>
2022-12-10 23:09:34 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,11 @@
+package CarryQuestion1;
+
+public class SumOfArray {
+	public static void main(String[] args) {
+		
+	float[] marks = {26.0f, 63.0f, 73.0f, 83.0f, 45.0f};
+	float sum =0;
+	for(float num : marks){
+		sum = sum + num ;}
+	System.out.println("the value of this"+ sum);
+}}
